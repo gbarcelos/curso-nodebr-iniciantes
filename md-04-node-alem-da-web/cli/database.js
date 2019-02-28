23cc1a96a3ca5ae56f4bdc025840f590665e198a
Gustavo Barcelos <gustavo.barcelos@bmgmoney.com>
2019-02-27 21:56:49 -0300

    Manipulando arquivos - CREATE

--- md-04-node-alem-da-web/cli/database.js
+++ md-04-node-alem-da-web/cli/database.js
@@ -18,6 +18,11 @@ class Database {
         return JSON.parse(arquivo.toString());
     }
 
+    async escreverArquivo(dados) {
+        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados));
+        return true;
+    }    
+
     async listar(id) {
 
         const dados = await this.obterDadosArquivo();
@@ -25,9 +30,42 @@ class Database {
         return dados.filter(item => (id ? item.id == id : true));
     }
 
-    async escreverArquivo(dados) {
+    async cadastrar(heroi) {
+        const dados = await this.obterDadosArquivo();
+        //workaround para simular um id
+        const id = heroi.id <= 2 ? heroi.id : Date.now();
+        
+        // concatena o id com o objeto heroi
+        const heroiComId = {
+          ...heroi,
+          id,
+        };
 
+        // concatena o heroiComId com o objeto dados
+        const dadosFinal = [
+            ...dados,
+            heroiComId
+        ];
+        return await this.escreverArquivo(dadosFinal);
     }
+
+    async remover(id) {
+        if (!id) {
+          await this.escreverArquivo([]);
+          return true;
+        }
+    
+        const dados = await this.obterDadosArquivo();
+    
+        const indice = dados.findIndex(item => item.id === parseInt(id));
+        if (indice === -1) {
+          throw Error('heroi não existe!');
+        }
+        const atual = dados[indice];
+        dados.splice(indice, 1);
+        await this.escreverArquivo(dados);
+        return true;
+      }
 
 }
 
--- md-04-node-alem-da-web/cli/test.js
+++ md-04-node-alem-da-web/cli/test.js
@@ -6,9 +6,23 @@ const DEFAULT_ITEM_CADASTRAR = { nome: 'Flash', poder: 'speed', id: 1 };
 
 describe('Suite de manipulação de herois', () => {
 
+    before(async () => {
+        await Database.remover();
+        await Database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    });
+
     it('deve pesquisar um heroi usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR;
         const [resultado] = await Database.listar(expected.id);
         deepEqual(resultado, expected);
+    });
+
+    it('deve cadastrar um heroi usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR;
+        const resultado = await Database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+
+        const [atual] = await Database.listar(DEFAULT_ITEM_CADASTRAR.id);
+
+        deepEqual(atual, expected);
     });
 });
